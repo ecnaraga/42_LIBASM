99f737b045eb0685dab9f70d8dba306026765bc0
Garance LAMBEY <galambey@bess-f3r1s15.clusters.42paris.fr>
2025-02-12 17:52:03 +0100

    ft_list_remove_if in progress

--- Makefile
+++ Makefile
@@ -14,7 +14,8 @@ SRCS = 	ft_strlen.s \
 SRCS_BONUS =	ft_atoi_base.s \
 				ft_list_push_front.s \
 				ft_list_size.s \
-				ft_list_sort.s
+				ft_list_sort.s \
+				ft_list_remove_if.s
 
 OBJS = $(SRCS:.s=.o)
 OBJS_BONUS = $(SRCS_BONUS:.s=.o)
--- test/cmain.c
+++ test/cmain.c
@@ -8,8 +8,9 @@ struct s_list *next;
 } t_list;
 
 void    ft_list_push_front(t_list **begin_list, void *data);
-int    ft_list_size(t_list *begin_list);
+int    	ft_list_size(t_list *begin_list);
 void    ft_list_sort(t_list **begin_list, int (*ft_cmp)());
+void    ft_list_remove_if(t_list **begin_list, void	*data_ref, int (*ft_cmp)(), void(*free_fct)(void *));
 
 t_list	*ft_lstnew(void *content)
 {
@@ -45,11 +46,15 @@ extern int ft_cmp(int data, int value) {
 	return 0;
 }
 
+extern void free_fct(void *elem __attribute__((unused))) {
+	printf("free\n");
+}
+
 int main(int ac __attribute__((unused)), char **av __attribute__((unused))) {
 
     t_list *begin = 0;
-	int array[] = {14};
-	for (int i = 0; i < 2; i++) {
+	int array[] = {10, 11, 12, 13};
+	for (int i = 0; i < 4; i++) {
 		void *data = ft_lstnew(&array[i]);
     	ft_list_push_front(&begin, data);
 	}
@@ -76,6 +81,14 @@ int main(int ac __attribute__((unused)), char **av __attribute__((unused))) {
 		begin = begin->next;
 	}
 	begin = tmp;
+
+	int a = 11;
+	ft_list_remove_if(&begin, &a, ft_cmp, free_fct);
+	printf("\n");
+	while (begin) {
+		printf("%d\n", *(int*)(begin->data));
+		begin = begin->next;
+	}
 }
 
 /*
